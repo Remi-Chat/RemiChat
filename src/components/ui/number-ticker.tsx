e799929f8d57b28736bf8070206fdee7faa71931
Aqua <86239876+Aqua-123@users.noreply.github.com>
2025-01-09 19:03:22 +0530

    Complete main page

--- src/components/ui/number-ticker.tsx
+++ src/components/ui/number-ticker.tsx
@@ -27,6 +27,7 @@ export default function NumberTicker({
   const isInView = useInView(ref, { once: true, margin: "0px" });
 
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     isInView &&
       setTimeout(() => {
         motionValue.set(direction === "down" ? 0 : value);
@@ -43,14 +44,14 @@ export default function NumberTicker({
           }).format(Number(latest.toFixed(decimalPlaces)));
         }
       }),
-    [springValue, decimalPlaces],
+    [springValue, decimalPlaces]
   );
 
   return (
     <span
       className={cn(
         "inline-block tabular-nums tracking-wider text-black dark:text-white",
-        className,
+        className
       )}
       ref={ref}
     />
